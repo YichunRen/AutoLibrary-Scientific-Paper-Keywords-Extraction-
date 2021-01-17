9d4bdff610159de89d275aaa239fb2d930b29212
Jiayi Fan <jif087@ucsd.edu>
2021-01-17 15:48:12 +0800

    add submodule init

--- run.py
+++ run.py
@@ -9,8 +9,10 @@ from convert import convert_txt
 
 def main(targets):
     data_config = json.load(open('config/data-params.json'))
+    model_config = json.load(open('config/model-params.json'))
+    eda_config = json.load(open('config/eda-params.json'))
 
-    #os.system('git submodule update --init')
+    os.system('git submodule update --init')
     
     if 'data' in targets:
         convert_txt(**data_config)
