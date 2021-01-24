73c7226b055c8ed93b0b12b1ddb0de6735627211
Jiayi Fan <jif087@ucsd.edu>
2021-01-25 00:22:02 +0800

    update views.py and index.html

--- website/website/settings.py
+++ website/website/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret! # the security key is saved in doc
-SECRET_KEY = 'wfu9-o-wre7ss%kl-4^e0iv(vb=5#loylpow66cb#r5orl0qlu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
