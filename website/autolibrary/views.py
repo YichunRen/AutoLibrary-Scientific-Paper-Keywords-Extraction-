a1cfd5014c37b03ab1d9c99ae0db6e30aa910b34
Ruby Ren <yir016@ucsd.edu>
2021-01-24 06:49:02 -0800

    update website

--- website/autolibrary/views.py
+++ website/autolibrary/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the AutoLibrary page.")
+    #return render(request, os.path.dirname(os.path.realpath(__file__)) + '/code/index.html')
