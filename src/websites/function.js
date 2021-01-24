22084dcbcdc4110acbbac02cf81a21b49635c939
Jiayi Fan <jif087@ucsd.edu>
2021-01-24 17:42:38 +0800

    update dropzone

--- src/websites/function.js
+++ src/websites/function.js
@@ -1,18 +1,23 @@
-function initiate_page() {
-    var dropzone = document.getElementById("dropzone");
-    dropzone.ondragover = dropzone.ondragenter = function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-    }
-
-    dropzone.ondrop = function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-
-        const filesArray = event.dataTransfer.files;
-        for (let i=0; i<filesArray.length; i++) {
-            sendFile(filesArray[i]);
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
         }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      }
     }
 }
 
