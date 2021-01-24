88b85020ad6a04b8833d42a2b542b38ae2fa8aa7
Jiayi Fan <jif087@ucsd.edu>
2021-01-24 17:09:16 +0800

    update dropzone

--- src/websites/function.js
+++ src/websites/function.js
@@ -1,5 +1,5 @@
 function initiate_page() {
-    const dropzone = document.getElementById("dropzone");
+    var dropzone = document.getElementById("dropzone");
     dropzone.ondragover = dropzone.ondragenter = function(event) {
         event.stopPropagation();
         event.preventDefault();
@@ -17,12 +17,28 @@ function initiate_page() {
 }
 
 
-
-// Trying-->
-function saveFile(){
-    fileObj = document.getElementById("input_pdf");
-    const objectURL = window.URL.createObjectURL(fileObj);
+// From online-->
+function saveTextAsFile(){
+    var textToSave = document.getElementById("inputTextToSave").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:".pdf"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+ 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
 }
+ 
+function destroyClickedElement(event){
+    document.body.removeChild(event.target);
+}
+
 
 // From online-->
 
