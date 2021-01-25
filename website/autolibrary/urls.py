f272bf00b4debc3d57abea02541f13eb6d4a560c
Jiayi Fan <jif087@ucsd.edu>
2021-01-25 22:33:06 +0800

    Update urls.py

--- website/autolibrary/urls.py
+++ website/autolibrary/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/a', views.get_file, name='get_file'),
-    #path('page<int:num>', views.get_file),
-    #url('/autolibrary', views.get_file),
+    path('a/', views.get_file, name='get_file'),
 ]
