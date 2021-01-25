28765e1a7e5825907aa86d7a0a911ee859cb33cf
Ruby Ren <yir016@ucsd.edu>
2021-01-25 05:54:48 -0800

    change pages

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'autolibrary.apps.AutolibraryConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
