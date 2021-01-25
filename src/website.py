38e6fd106f1501fe7db725e1f1faff14ead1e279
Ruby Ren <yir016@ucsd.edu>
2021-01-25 06:16:15 -0800

    auto open browser

--- src/website.py
+++ src/website.py
@@ -1,4 +1,6 @@
 import os
+import platform
+
 
 def activate_website(key, address):
     print("\n")
@@ -7,6 +9,7 @@ def activate_website(key, address):
     with open('website/website/settings.py', 'r') as f:
         last_line = f.readlines()[-1]
         
+    system_name = platform.system()    
     print("  => Starting the Django development server...")
     with open('src/website.sh', 'w') as rsh:
         rsh.write('''cd website/website \n''')
@@ -15,6 +18,12 @@ def activate_website(key, address):
             rsh.write(key)
             rsh.write('''" >> settings.py \n''')
         rsh.write('''cd .. \n''')
-        rsh.write('''python manage.py runserver \n''')
+        if system_name == 'Darwin':
+            rsh.write('''open "http://127.0.0.1:8000/autolibrary" && python manage.py runserver \n''')
+        elif system_name == 'Windows':
+            rsh.write('''explorer "http://127.0.0.1:8000/autolibrary" && python manage.py runserver \n''')
+        else:
+            rsh.write('''python -m webbrowser "http://127.0.0.1:8000/autolibrary" && python manage.py runserver \n''')
+
     os.system('bash src/website.sh')
     return
