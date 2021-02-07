4583e27d248267f6809c5a90bd0f927a8b00b142
Jiayi Fan <jif087@ucsd.edu>
2021-02-07 15:11:35 +0800

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def main(targets):
     if 'website' in targets:
         activate_website(**website_config)
     if 'test' in targets:
-        convert_txt(**test_config)
+        convert_txt(test_config['indir'], data_config['outdir'], test_config['pdfname'],)
         autophrase(data_config['outdir'], test_config['pdfname'], model_config['outdir'], model_config['filename'])
         change_weight(**weight_config)
         webscrape(webscrape_config['keywords_path'], test_config['fos_path'], webscrape_config['out_path'])
