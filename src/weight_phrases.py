a56cf138e1e0852baead56dfa6c003161a00d208
Ruby Ren <yir016@ucsd.edu>
2021-02-09 01:55:26 -0800

    Update weight_phrases.py

--- src/weight_phrases.py
+++ src/weight_phrases.py
@@ -8,13 +8,16 @@ def change_weight(paper_ap_path, selected_domain, domain_ap_path, out_path, keyw
     print("  => Altering quality scores of phrases in the input document according to domain...")
     
     try:
-        with open('src/domain_abb.json') as f:
+        with open('src/domains_abb.json') as f:
             domains_name_dict = json.load(f)
-        with open('data/out/selected_domain.txt') as f2:
-            selected_domain = domains_name_dict[f2.readline()]
-            domain_full_path = domain_ap_path + selected_domain + '/AutoPhrase.txt'
+            with open('data/out/selected_domain.txt') as f2:
+                selected_domain = domains_name_dict[f2.readline()]
+                
+                domain_full_path = domain_ap_path + selected_domain + '/AutoPhrase.txt'
+                print('Read domains phrases from '+ domain_full_path)
     except:
         domain_full_path = domain_ap_path + selected_domain + '/AutoPhrase.txt'
+        print('Read domains phrases from '+ domain_full_path)
 
     data = pd.read_csv(paper_ap_path, sep="\t", header = None, names=["score", "phrase"])
     data_domain = pd.read_csv(domain_full_path, sep="\t", header = None, names=["score", "phrase"])
