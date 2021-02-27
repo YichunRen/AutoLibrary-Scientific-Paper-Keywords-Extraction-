cfb6d79083fb9c0ea26b213de28a6f70988bf06f
Jiayi Fan <jif087@ucsd.edu>
2021-02-27 20:46:55 +0800

    add in_queue and timestamp

--- website/autolibrary/views.py
+++ website/autolibrary/views.py
@@ -6,6 +6,10 @@ import pandas as pd
 import subprocess
 from json import dumps 
 from django.views.decorators.csrf import csrf_exempt
+import time
+import datetime
+from django.utils import timezone
+from django.contrib.sessions.models import Session
 
 # if_customized = "true"
 # selected_doc = ''
@@ -27,16 +31,14 @@ def index(request):
     shared_obj['selected_subdomain'] = ''
     shared_obj['selected_keywords'] = ''
     shared_obj['phrases'] = []
-
+    shared_obj['in_queue'] = "false"
+    shared_obj['timestamp'] = ''
     request.session['myobj'] = shared_obj
 
     return render(request, 'autolibrary/index.html', {"data": data})
 
 def result(request):
     data = os.listdir('autolibrary/documents')
-    from pathlib import Path
-    print("File      Path:", Path(__file__).absolute())
-    #domains = json.load(open('../config/domains_full.json'))
     domains = json.load(open('../config/domains_full.json'))
 
     #new
@@ -52,6 +54,8 @@ def result(request):
         "domains": dumps(domains)
     }
     
+    shared_obj['in_queue'] = "false"
+    request.session['myobj'] = shared_obj
     return render(request, 'autolibrary/result.html', content)
 
 def customization(request):
@@ -85,16 +89,15 @@ def customization(request):
         if_customized = "true"
         shared_obj['if_customized'] = if_customized
 
+    shared_obj['in_queue'] = "false"
     request.session['myobj'] = shared_obj
-
     return render(request, 'autolibrary/customization.html', content)
 
 @csrf_exempt
 def get_file(request):
     if request.method == 'POST':
-        shared_obj = request.session['myobj']
-
         if "file_name" in request.POST:
+            shared_obj = request.session['myobj']
             #global if_customized
             if_customized = "false"
             shared_obj['if_customized'] = if_customized
@@ -116,20 +119,18 @@ def get_file(request):
             command = 'cp autolibrary/documents_copy/' + pdfname + ' static/autolibrary/documents'
             os.system(command)
 
+            shared_obj['in_queue'] = "false"
             request.session['myobj'] = shared_obj
             return HttpResponse('get file')
-
-    request.session['myobj'] = shared_obj
     return HttpResponse('fail to get file')
 
 @csrf_exempt
 def get_domain(request): 
-    shared_obj = request.session['myobj'] 
-
     if request.method == 'POST':
         if "domain" in request.POST:
+            shared_obj = request.session['myobj'] 
             # save selected domain to data/out
-            #global selected_pdf, selected_domain, selected_subdomain
+            # global selected_pdf, selected_domain, selected_subdomain
             selected_domain = request.POST['domain']
             selected_subdomain = request.POST['subdomain']
             selected_pdf = shared_obj['selected_pdf']
@@ -148,6 +149,28 @@ def get_domain(request):
             config = {'fos': [selected_domain]}
             with open('../data/out/fos_' + unique_key + '.json', 'w') as fp:
                 json.dump(config, fp)
+
+            # set in_queue and timestamp
+            shared_obj['in_queue'] = "true"
+            d = datetime.datetime.now(datetime.timezone.utc)
+            unix = time.mktime(d.timetuple())
+            shared_obj['timestamp'] = unix
+            # calculate wait time
+            sessions = Session.objects.filter(expire_date__gte=timezone.now())
+            sessions_in_queue = []
+            for session in sessions:
+                s = session.get_decoded()
+                if session.session_key != unique_key:
+                    if 'in_queue' in s.keys() and s['in_queue'] == "true":
+                        sessions_in_queue.append(s['timestamp'])
+            sessions_in_queue.sort()
+            wait_time = 0
+            for timestamp in sessions_in_queue:
+                if timestamp <= unix:
+                    wait_time += 60
+                else: 
+                    break
+            time.sleep(wait_time)
 
             # rewrite data-params.json
             config = json.load(open('../config/data-params.json'))
@@ -171,23 +194,23 @@ def get_domain(request):
                 rsh.write('''cp data/out/scraped_AutoPhrase.json website/static/autolibrary/web_scrap/scraped_AutoPhrase.json \n''')
             process = subprocess.Popen(['bash', 'autolibrary/run.sh'])
             process.wait()
-            #os.system('bash autolibrary.run.sh')
+
             # display phrases with a weighted quality score > 0.5
             # global phrases
             data = pd.read_csv('../data/out/weighted_AutoPhrase.csv', index_col = "Unnamed: 0")
             phrases = data[data['score'] > 0.5]['phrase'].to_list()
             shared_obj['phrases'] = phrases 
 
+            shared_obj['in_queue'] = "false"
             request.session['myobj'] = shared_obj
             return HttpResponse('get domain')
-    request.session['myobj'] = shared_obj
     return HttpResponse('fail to get domain')
 
 @csrf_exempt
 def get_keywords(request):  
-    shared_obj = request.session['myobj'] 
     if request.method == 'POST':
         if "keywords" in request.POST:
+            shared_obj = request.session['myobj'] 
             # save selected keywords to data/out
             # global selected_keywords
             selected_keywords = request.POST['keywords']
@@ -202,7 +225,8 @@ def get_keywords(request):
                 rsh.write('''cp data/out/scraped_AutoPhrase.json website/static/autolibrary/web_scrap/scraped_AutoPhrase.json''')
             process = subprocess.Popen(['bash', 'autolibrary/run.sh'])
             process.wait()
+
+            shared_obj['in_queue'] = "false"
             request.session['myobj'] = shared_obj
             return HttpResponse('get keywords')
-    request.session['myobj'] = shared_obj
     return HttpResponse('fail to get keywords')
